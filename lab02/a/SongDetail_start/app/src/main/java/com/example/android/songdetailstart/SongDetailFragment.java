db70b2f1b4cce26e7c1fe37c1824838083b1ff9c
Raqsf <raquelsf@ua.pt>
2022-05-19 23:20:43 +0100

    a) done

---
+++ lab02/a/SongDetail_start/app/src/main/java/com/example/android/songdetailstart/SongDetailFragment.java
@@ -0,0 +1,62 @@
+package com.example.android.songdetailstart;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.songdetailstart.content.SongUtils;
+
+public class SongDetailFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    // SongItem includes the song title and detail.
+    public SongUtils.Song mSong;
+
+    public SongDetailFragment() {
+        // Required empty public constructor
+    }
+
+    public static SongDetailFragment newInstance(int selectedSong) {
+        SongDetailFragment fragment = new SongDetailFragment();
+        // Set the bundle arguments for the fragment.
+        Bundle arguments = new Bundle();
+        arguments.putInt(SongUtils.SONG_ID_KEY, selectedSong);
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments().containsKey(SongUtils.SONG_ID_KEY)) {
+            // Load the content specified by the fragment arguments.
+            mSong = SongUtils.SONG_ITEMS.get(getArguments()
+                    .getInt(SongUtils.SONG_ID_KEY));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView =
+                inflater.inflate(R.layout.song_detail, container, false);
+
+        if (mSong != null) {
+            ((TextView) rootView.findViewById(R.id.song_detail))
+                    .setText(mSong.details);
+        }
+
+        return rootView;
+    }
+}
